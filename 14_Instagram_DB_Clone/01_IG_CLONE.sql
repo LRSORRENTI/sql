18a4def20e0733da14c51710a204ca8298556587
Luke <lrsorrenti@hotmail.com>
2024-04-20 09:33:07 -0500

    added follows schema and insert

--- 14_Instagram_DB_Clone/01_IG_CLONE.sql
+++ 14_Instagram_DB_Clone/01_IG_CLONE.sql
@@ -160,3 +160,40 @@ INSERT INTO likes (user_id, photo_id) VALUES
 
 -- INSERT INTO likes(user_id, photo_id) VALUES 
 -- (1,1);
+
+-- FOLLOWERS SCHEMA 
+
+-- How will this work? If I follow a user, then they can 
+-- also follow back, or not, how would we keep track of 
+-- this? 
+
+-- Essentially we need two follower id', both are foreign 
+-- keys referring to a user id, so follower_id of 1 follows 
+-- follower_id of 2
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+INSERT INTO follows (follower_id, followee_id) 
+VALUES 
+(1, 2),
+(2, 3),
+(3, 1),
+(2, 1);
+
+SELECT * FROM follows;
+
+-- +-------------+-------------+---------------------+
+-- | follower_id | followee_id | created_at          |
+-- +-------------+-------------+---------------------+
+-- |           1 |           2 | 2024-04-20 09:31:40 |
+-- |           2 |           1 | 2024-04-20 09:31:40 |
+-- |           2 |           3 | 2024-04-20 09:31:40 |
+-- |           3 |           1 | 2024-04-20 09:31:40 |
+-- +-------------+-------------+---------------------+
